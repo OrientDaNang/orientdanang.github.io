e0592e8b1a9dce0488fbb7b737da5d63a4c87c67
OrientDaNang <68224087+OrientDaNang@users.noreply.github.com>
2020-07-16 17:06:14 +0700

    add logo 2

--- docs/conf.py
+++ docs/conf.py
@@ -41,8 +41,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Orient Software'
-copyright = u'2020, Test 1 '
+project = u'Orient Da Nang'
+copyright = u'2020, Orient Software '
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -114,7 +114,7 @@ html_theme_options = {
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = "image/logo.png"
+html_logo = "images/logo.png"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
